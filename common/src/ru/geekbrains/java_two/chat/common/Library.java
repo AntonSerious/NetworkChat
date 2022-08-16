012e3321d541a0331d812b2b37d5140b7bf4d374
anemchenko <anemchenko@nes.ru>
2022-08-16 16:04:22 +0300

    убрал лишние комментарии

--- common/src/ru/geekbrains/java_two/chat/common/Library.java
+++ common/src/ru/geekbrains/java_two/chat/common/Library.java
@@ -17,7 +17,7 @@ public class Library {
     public static final String AUTH_REQUEST = "/auth_request";
     public static final String AUTH_ACCEPT = "/auth_accept";
     public static final String AUTH_DENIED = "/auth_denied";
-    public static final String MSG_FORMAT_ERROR = "/msg_format_error"; // если мы вдруг не поняли, что за сообщение и не смогли разобрать
+    public static final String MSG_FORMAT_ERROR = "/msg_format_error"; // если сервер не понял, что за сообщение и не смог разобрать
     public static final String TYPE_BROADCAST = "/bcast"; // то есть сообщение, которое будет посылаться всем
     public static final String USER_LIST = "/user_list";
     public static final String TYPE_BCAST_CLIENT = "/client_bcast";
@@ -63,8 +63,4 @@ public class Library {
 
         return TYPE_ECHO_PRIVATE_CLIENT + DELIMITER + System.currentTimeMillis() + DELIMITER + src+ DELIMITER+ toNickname + DELIMITER+ msg;
     }
-
-
-
-
 }
